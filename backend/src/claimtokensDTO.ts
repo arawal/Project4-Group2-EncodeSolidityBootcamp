950e4fd0cf552bd0355c2a77c1ef21e95d9b1122
Karen J Yang <karenjyang.1@gmail.com>
2022-11-26 16:52:08 -0600

    added comments

--- backend/src/claimtokensDTO.ts
+++ backend/src/claimtokensDTO.ts
@@ -1,6 +1,9 @@
 export class claimTokensDTO {
-    // request voting tokens form API passing my address
+    // request voting tokens from API passing my address, open-ended part, put in separate file
     address: string;
+    // amount: number;
+    // signature: string;
+  
     constructor(address: string) {
       this.address = address;
     }
